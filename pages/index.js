08f3b765df607ede25c11797a893f8a12f0f2193
Laylson Sousa <laylsonss3@gmail.com>
2022-04-23 19:42:15 -0300

    preview 6

--- pages/index.js
+++ pages/index.js
@@ -17,11 +17,11 @@ function Home() {
         <div className="menu-e-logo">
             <div>
                 <label><a className="menu-nav" href="/">Inicio</a></label>
-                <label><a className="menu-nav" href="#">Galeria</a></label>
-                <label><a className="menu-nav" href="#">Quem Somos</a></label>
-                <label><a className="menu-nav" href="#">Cursos</a></label>
-                <label><a className="menu-nav" href="#">Apoie</a></label>
-                <label><a className="menu-nav" href="#">Contato</a></label>
+                <label><a className="menu-nav" href="#galeria">Galeria</a></label>
+                <label><a className="menu-nav" href="#quem-somos">Quem Somos</a></label>
+                <label><a className="menu-nav" href="#cursos">Cursos</a></label>
+                <label><a className="menu-nav" href="#apoie">Apoie</a></label>
+                <label><a className="menu-nav" href="#contato">Contato</a></label>
             </div>
             <div>
                 <img className="logo" src='/images/logo-soarte.svg' alt="Logo Soarte"/>
@@ -31,7 +31,7 @@ function Home() {
 </header>
 
 <main>
-    <section className="galeria">
+    <section className="galeria" id="galeria">
             <h1>Galeria</h1>
             <img className="foto-galeria" src='/images/paulo.jpg' alt="Professor Paulo Ferreira"/>
             <img className="foto-galeria" src='/images/luciano.jpg' alt="Professor Luciano Pereira"/>
@@ -40,7 +40,7 @@ function Home() {
             <a className="button-galeria" href="#">Ver mais...</a>
     </section>
 
-    <section className="quem-somos">
+    <section className="quem-somos" id="quem-somos">
         <h1>
             Quem somos
         </h1>
@@ -59,7 +59,7 @@ function Home() {
         generated Lorem Ipsum.
         </p>
     </section>
-    <section className="cursos">
+    <section className="cursos" id="cursos">
             <h1>Cursos</h1>
             <label><a href="#"><img className="button-curso" src="/images/flute.png" alt="Flauta Doce" /></a></label>
             <label><a href="#"><img className="button-curso" src="/images/transverse-flute.png" alt="Flauta Transversal" /></a></label>
@@ -71,20 +71,40 @@ function Home() {
             <label><a href="#"><img className="button-curso" src="/images/drum.png" alt="Percussão" /></a></label>
             <label><a href="#"><img className="button-curso" src="/images/pc.png" alt="Informática" /></a></label>
     </section>
-    <section className="apoie">
+    <section className="apoie" id="apoie">
             <h1>Apoie</h1>
-            <a className="button-apoie" href="#">APOIE!</a>
+            <a className="button-apoie" href="https://apoia.se/soarte" target="_BLANK">APOIE!</a>
             <p style={{fontSize:'14pt'}}>Nos ajude a continuar <span style={{fontWeight:'900'}}>transformando vidas</span> através da <span style={{fontWeight:'900'}}>arte!</span></p>
     </section>
-    <section className="contato">
-            <h1>Contato</h1>
-            <a></a>
+    <section className="contato" id="contato">
+            <div style={{width:'320px', margin:'auto'}}>
+                <h2 style={{paddingBottom:'15px'}}>
+                    Endereço da Soarte
+                </h2>
+                <p>
+                    Rua Antonio Luiz de Sousa, 560 - Monguba - Pacatuba-CE - CEP 61.805-770<br />
+                    E-mail: soartemusica@gmail.com<br />
+                    CNPJ: 10.967.869/0001-63<br />
+                    Telefone: (85) 9 9129-2607
+                </p>
+            </div>
+            <div className="form-contato">
+                    <h1>Contato</h1>
+                <form style={{width:'350px',margin:'auto'}} autoComplete="on">
+                    <label>Nome: </label>
+                    <input name="nome" type="text" size="30" /><br />
+                    <label>E-mail: </label>
+                    <input name="email" type="text" size="30" /><br />
+                    <label>Mensagem: </label><br />
+                    <textarea name="message" rows="4" cols="38" /><br />
+                    <input type="submit"></input>
+                </form>
+            </div>
     </section>
 </main>
 <footer>
-        <h1 style={{textAlign:'center',}}>Rodapé</h1>
+        <h1 style={{textAlign:'center',}}></h1>
 </footer>
-
 </div>
     )
 }
