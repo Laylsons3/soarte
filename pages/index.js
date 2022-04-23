11cbb4e8e0f6f046d5ec19d39ea6d74b361999d7
Laylson Sousa <laylsonss3@gmail.com>
2022-04-23 16:21:43 -0300

    preview 3

--- pages/index.js
+++ pages/index.js
@@ -36,10 +36,30 @@ function Home() {
             <img className="foto-galeria" src='/images/paulo.jpg' alt="Professor Paulo Ferreira"/>
             <img className="foto-galeria" src='/images/luciano.jpg' alt="Professor Luciano Pereira"/>
             <img className="foto-galeria" src='/images/manoel.jpg' alt="Professor Manoel Teodósio"/>
+    </section>
 
+    <section className="quem-somos">
+        <h1>
+            Quem somos
+        </h1>
+        There are many variations of passages of Lorem Ipsum available, but the majority have suffered alteration in some form, by injected humour, or randomised words which don't look even slightly believable. If you are going to use a passage of Lorem Ipsum, you need to be sure there isn't anything embarrassing hidden in the middle of text. All the Lorem Ipsum generators on the Internet tend to repeat predefined chunks as necessary, making this the first true generator on the Internet. It uses a dictionary of over 200 Latin words, combined with a handful of model sentence structures, to generate Lorem Ipsum which looks reasonable. The generated Lorem Ipsum.
+    </section>
+    <section className="cursos">
+            <h1>Cursos</h1>
+            <a></a>
+    </section>
+    <section className="apoie">
+            <h1>Apoie</h1>
+            <a></a>
+    </section>
+    <section className="contato">
+            <h1>Contato</h1>
+            <a></a>
     </section>
 </main>
-
+<footer>
+        <h1 style={{textAlign:'center',}}>Rodapé</h1>
+</footer>
 
 </div>
     )
