89d47906e06e9525246ab39aacb2b2b0af053d4d
Laylson Sousa <laylsonss3@gmail.com>
2022-04-26 22:20:25 -0300

    v1.0

--- components/Cursos.js
+++ components/Cursos.js
@@ -3,22 +3,28 @@ function Home() {
 <div>
 
 <section className="cursos" id="cursos">
+
+
             <h1>Cursos</h1>
             <div className="div-cursos">
-            <label className="div-button-curso"><a href="#cursos"><div className="div-button-curso"><img className="button-curso" src="/images/flute.png" alt="Flauta Doce" /></div></a><span className="spa">Flauta-Doce</span></label>
-            <label className="div-button-curso"><a href="#cursos"><div className="div-button-curso"><img className="button-curso" src="/images/transverse-flute.png" alt="Flauta Transversal" /></div></a><span className="spa">Flauta Transversal</span></label>
-            <label className="div-button-curso"><a href="#cursos"><div className="div-button-curso"><img className="button-curso" src="/images/clarinet.png" alt="Clarinete" /></div></a><span className="spa">Clarinete</span></label>
-            <label className="div-button-curso"><a href="#cursos"><div className="div-button-curso"><img className="button-curso" src="/images/guitar.png" alt="Violão" /></div></a><span className="spa">Violão</span></label>
-            <label className="div-button-curso"><a href="#cursos"><div className="div-button-curso"><img className="button-curso" src="/images/sax.png" alt="Saxofone" /></div></a><span className="spa">Saxofone</span></label>
-            <label className="div-button-curso"><a href="#cursos"><div className="div-button-curso"><img className="button-curso" src="/images/trumpet.png" alt="Metais" /></div></a><span className="spa">Metais</span></label>
-            <label className="div-button-curso"><a href="#cursos"><div className="div-button-curso"><img className="button-curso" src="/images/voice.png" alt="Canto" /></div></a><span className="spa">Canto</span></label>
-            <label className="div-button-curso"><a href="#cursos"><div className="div-button-curso"><img className="button-curso" src="/images/drum.png" alt="Percussão" /></div></a><span className="spa">Percussão</span></label>
-            <label className="div-button-curso"><a href="#cursos"><div className="div-button-curso"><img className="button-curso" src="/images/pc.png" alt="Informática" /></div></a><span>Informática</span></label>
+            <a href="#cursos"><img className="button-curso" src="/images/flute.png" /></a>
+            <a href="#cursos"><img className="button-curso" src="/images/transverse-flute.png" /></a>
+            <a href="#cursos"><img className="button-curso" src="/images/clarinet.png" /></a>
+            <a href="#cursos"><img className="button-curso" src="/images/trumpet.png" /></a>
+            <a href="#cursos"><img className="button-curso" src="/images/guitar.png" /></a>
+            <a href="#cursos"><img className="button-curso" src="/images/sax.png" /></a>
+            <a href="#cursos"><img className="button-curso" src="/images/voice.png" /></a>
+            <a href="#cursos"><img className="button-curso" src="/images/drum.png" /></a>
+            <a href="#cursos"><img className="button-curso" src="/images/pc.png" /></a>
+
+
             </div>
     </section>
 
 </div>
     )
 }
+
+
 
 export default Home
