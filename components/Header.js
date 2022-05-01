57a0303bdc41bbe206801da42ed1a1164ae96897
Laylson Sousa <laylsonss3@gmail.com>
2022-04-30 23:23:14 -0300

    v1.3.5

--- components/Header.js
+++ components/Header.js
@@ -14,11 +14,11 @@ function Home() {
                 <input type="checkbox" id="drop" />
                     <ul class="menu">
                         <li><a href="/">início</a></li>
-                        <li><a href="#">galeria</a></li>
-                        <li><a href="#">cursos</a></li>
-                        <li><a href="#">quem somos</a></li>
-                        <li><a href="#">apoie</a></li>
-                        <li><a style={{backgroundColor:'green',padding:'2px 10px',borderRadius:'15px'}} href="#">contato</a></li>
+                        <li><a href="/galeria">galeria</a></li>
+                        <li><a href="#cursos">cursos</a></li>
+                        <li><a href="#quem-somos">quem somos</a></li>
+                        <li><a href="#apoie">apoie</a></li>
+                        <li><a style={{backgroundColor:'green',padding:'2px 10px',borderRadius:'15px'}} href="#contato">contato</a></li>
             </ul>
         </nav>
             </div>
